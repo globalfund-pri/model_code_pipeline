e94b527cf6dfae9bfd16cc128183fa74a3e1817b
MikaelaSmitGlobalFund <94458847+MikaelaSmitGlobalFund@users.noreply.github.com>
2024-07-18 11:27:55 +0200

    Added a test script to find errors and fixed the CIs

---
+++ src/scripts/ic7/analyses/board_analysis/investigate_ci_issues.py
@@ -0,0 +1,66 @@
+from scripts.ic7.tb.tb_analysis import get_tb_database
+from tgftools.analysis import Analysis
+from tgftools.filehandler import Parameters, TgfFunding, NonTgfFunding
+from tgftools.utils import get_root_path, get_data_path
+
+project_root = get_root_path()
+path_to_data_folder = get_data_path()
+funding_path = path_to_data_folder / "IC7" / 'TimEmulationTool' / "funding"
+
+
+# Declare the parameters, indicators and scenarios
+parameters = Parameters(project_root / "src" / "scripts" / "ic7" / "shared" / "parameters.toml")
+
+tb_db = get_tb_database(load_data_from_raw_files=False)
+
+# Load assumption for budgets for this analysis
+tgf_funding = (
+    TgfFunding(
+        path_to_data_folder
+        / "IC7/TimEmulationTool"
+        / "funding"
+        / "tb"
+        / "tgf"
+        / "tb_Fubgible_gf_17b_incUnalloc.csv"
+    )
+)
+non_tgf_funding = (
+    NonTgfFunding(
+        path_to_data_folder
+        / "IC7/TimEmulationTool"
+        / "funding"
+        / "tb"
+        / "non_tgf"
+        / "tb_nonFubgible_dipiBase.csv"
+    )
+)
+
+
+a_inn_on = Analysis(
+        database=tb_db,
+        scenario_descriptor='IC_IC',
+        tgf_funding=tgf_funding,
+        non_tgf_funding=non_tgf_funding,
+        parameters=parameters,
+        handle_out_of_bounds_costs=True,
+        innovation_on=True,
+    ).portfolio_projection_approach_a()
+
+a_inn_off = Analysis(
+        database=tb_db,
+        scenario_descriptor='IC_IC',
+        tgf_funding=tgf_funding,
+        non_tgf_funding=non_tgf_funding,
+        parameters=parameters,
+        handle_out_of_bounds_costs=True,
+        innovation_on=False,
+    ).portfolio_projection_approach_a()
+
+
+df_inn_off = a_inn_off.portfolio_results['cases']
+df_inn_on = a_inn_on.portfolio_results['cases']
+
+df_inn_off_d = a_inn_off.portfolio_results['deathshivneg']
+df_inn_on_d = a_inn_on.portfolio_results['deathshivneg']
+
+
