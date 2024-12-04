2deeca3bc12d5e3f8e62793c68057c2becc81865
MikaelaSmitGlobalFund <94458847+MikaelaSmitGlobalFund@users.noreply.github.com>
2024-12-04 14:26:35 +0100

    add functionality to extract post-optimized costs. ready for comparison

--- src/scripts/ic8/hiv/hiv_analysis.py
+++ src/scripts/ic8/hiv/hiv_analysis.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pandas as pd
+
 from scripts.ic8.hiv.hiv_checks import DatabaseChecksHiv
 from scripts.ic8.hiv.hiv_filehandlers import ModelResultsHiv, PFInputDataHIV, PartnerDataHIV, GpHiv
 from scripts.ic8.shared.create_frontier import filter_for_frontier
@@ -89,8 +91,10 @@ def get_hiv_database(load_data_from_raw_files: bool = True) -> Database:
 
     # Create and return the database
     return Database(
-        model_results=filter_for_frontier(model_results),
+        # These model results take the full cost impact curve as is
         # model_results=model_results,
+        # These model results are limited to the points of the cost-impact curve that are on the frontier
+        model_results=filter_for_frontier(model_results),
         gp=gp,
         pf_input_data=pf_input_data,
         partner_data=partner_data,
@@ -159,7 +163,7 @@ def get_hiv_analysis(
         tgf_funding=tgf_funding,
         non_tgf_funding=non_tgf_funding,
         parameters=parameters,
-        handle_out_of_bounds_costs=False,
+        handle_out_of_bounds_costs=True,
         innovation_on=False,
     )
 
@@ -174,10 +178,14 @@ if __name__ == "__main__":
         do_checks=DO_CHECKS
     )
 
-    analysis.make_diagnostic_report(optimisation_params={
+    # Make diagnostic report
+    analysis.make_diagnostic_report(
+        optimisation_params={
                 'years_for_obj_func': analysis.parameters.get('YEARS_FOR_OBJ_FUNC'),
                 'force_monotonic_decreasing': True,
-            }, methods=['ga_backwards', 'ga_forwards', ], provide_best_only=False, filename=Path("diagnostic_report_hiv.pdf"))
+            }, methods=['ga_backwards', 'ga_forwards', ], provide_best_only=False,
+        filename=get_root_path() / "outputs" / "diagnostic_report_hiv.pdf"
+    )
 
     # To examine results from approach A / B....
     # analysis.portfolio_projection_approach_a()
@@ -187,4 +195,19 @@ if __name__ == "__main__":
     # Get the finalised Set of Portfolio Projections (decided upon IC scenario and Counterfactual):
     from scripts.ic8.analyses.main_results_for_investment_case import get_set_of_portfolio_projections
     pps = get_set_of_portfolio_projections(analysis)
-    a = 3
+
+    # Portfolio Projection Approach B: to find optimal allocation of TGF
+    results_from_approach_b = analysis.portfolio_projection_approach_b(
+        optimisation_params={
+            'years_for_obj_func': analysis.parameters.get('YEARS_FOR_OBJ_FUNC'),
+            'force_monotonic_decreasing': True,
+        }, methods=['ga_backwards', 'ga_forwards', ]
+    )
+
+    (
+        pd.Series(results_from_approach_b.tgf_funding_by_country) + pd.Series(results_from_approach_b.non_tgf_funding_by_country)
+    ).to_csv(
+        get_root_path() / 'outputs' / 'hiv_tgf_optimal_allocation.csv',
+        header=False
+    )
+
