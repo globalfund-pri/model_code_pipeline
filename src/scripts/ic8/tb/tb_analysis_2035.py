c0b55b7033b42ff5117ac6bbb5e8d3a015d0fca8
MikaelaSmitGlobalFund <94458847+MikaelaSmitGlobalFund@users.noreply.github.com>
2024-12-05 12:40:49 +0100

    Add Tb results up to 2035

---
+++ src/scripts/ic8/tb/tb_analysis_2035.py
@@ -0,0 +1,204 @@
+from pathlib import Path
+
+import pandas as pd
+
+from scripts.ic8.shared.create_frontier import filter_for_frontier
+from scripts.ic8.tb.tb_checks import DatabaseChecksTb
+from scripts.ic8.tb.tb_filehandlers import PartnerDataTb, PFInputDataTb, ModelResultsTb, GpTb
+from tgftools.analysis import Analysis
+from tgftools.database import Database
+from tgftools.filehandler import (
+    FixedGp,
+    NonTgfFunding,
+    Parameters,
+    TgfFunding,
+)
+from tgftools.utils import (
+    get_data_path,
+    get_root_path,
+    load_var,
+    save_var,
+)
+
+"""This file holds everything relating to processing the model output. 
+It contains information on: 
+- The location of the parameter file
+- The location to the raw model output file and the location the model output should be saved to
+- Alternatively, if if the options is set to not reloading the raw data, the location of the file containing the loaded
+  model output which has been processed in the filehandler
+- The location to pf, partner and gp data and where to save the output of the gp file
+
+
+It also sets the following options: 
+- Whether to load the model output from raw (see LOAD_DATA_FROM_RAW_FILES at the bottom of the file). 
+  CAUTION: Updated to the filehandler relating to model output will not be reflected if this option is set to "False". 
+- Whether to run checks or not (see DO_CHECKS at the bottom of the file) and, if checks are to be run, where to save the
+  report of the checks. 
+- Options to set the tgf and non-tgf funding amounts to be used in the analysis. This includes an option to include or 
+  exclude unallocated amounts. This information has to be computed outside the MCP (set in the disease-specific analysis 
+  scripts when loading the budget assumptions) (see tgf_funding and non_tgf_funding). 
+- Which scenario should be used to compute the main investment case scenario (see scenario_descriptor). 
+
+NOTE: Scenarios for the various counterfactuals are set in the HTM class, and disease-specific CFs are set within the
+analysis class directly. 
+"""
+
+
+def get_tb_database(load_data_from_raw_files: bool = True) -> Database:
+
+    path_to_data_folder = get_data_path()
+    project_root = get_root_path()
+
+    # Declare the parameters, indicators and scenarios
+    parameters = Parameters(project_root / "src" / "scripts" / "ic8" / "shared" / "parameters.toml")
+
+    # Change end year
+    parameters.int_store['END_YEAR'] = 2035
+
+    if load_data_from_raw_files:
+        # Load the files
+        model_results = ModelResultsTb(
+            path_to_data_folder / "IC8/modelling_outputs/tb/2024_10_15",
+            parameters=parameters,
+        )
+        # Save the model_results object
+        save_var(model_results, project_root / "sessions" / "tb_model_data_2035_ic8.pkl")
+    else:
+        # Load the model results
+        model_results = load_var(project_root / "sessions" / "tb_model_data_2035_ic8.pkl")
+
+    # Load the files
+    pf_input_data = PFInputDataTb(
+        path_to_data_folder / "IC8/pf/tb/2024_03_28",
+        parameters=parameters
+    )
+
+    partner_data = PartnerDataTb(
+        path_to_data_folder / "IC8/partner/tb/2024_10_17",
+        parameters=parameters,
+    )
+
+    fixed_gp = FixedGp(
+        get_root_path() / "src" / "scripts" / "ic8" / "shared" / "fixed_gps" / "tb_gp.csv",
+        parameters=parameters,
+    )
+
+    gp = GpTb(
+        fixed_gp=fixed_gp,
+        model_results=model_results,
+        partner_data=partner_data,
+        parameters=parameters,
+    )
+
+    # Create and return the database
+    return Database(
+        # These model results take the full cost impact curve as is
+        # model_results=model_results,
+        # These model results are limited to the points of the cost-impact curve that are on the frontier
+        model_results=filter_for_frontier(model_results),
+        gp=gp,
+        pf_input_data=pf_input_data,
+        partner_data=partner_data,
+    )
+
+def get_tb_analysis(
+        load_data_from_raw_files: bool = True,
+        do_checks: bool = False,
+) -> Analysis:
+    """Return the Analysis object for TB."""
+
+    path_to_data_folder = get_data_path()
+    project_root = get_root_path()
+
+    # Declare the parameters, indicators and scenarios
+    parameters = Parameters(project_root / "src" / "scripts" / "ic8" / "shared" / "parameters.toml")
+
+    db = get_tb_database(load_data_from_raw_files=load_data_from_raw_files)
+
+    # Run the checks
+    if do_checks:
+        DatabaseChecksTb(
+            db=db,
+            parameters=parameters
+        ).run(
+            suppress_error=True,
+            filename=project_root / "outputs" / "tb_report_of_checks_ic8.pdf"
+        )
+
+    # Load assumption for budgets for this analysis
+    tgf_funding = (
+        TgfFunding(
+            path_to_data_folder
+            / "IC8"
+            / "funding"
+            / "2024_11_24"
+            / "tb"
+            / "tgf"
+            / "tb_fung_inc_unalc_bs17.csv"
+        )
+    )
+
+    list = parameters.get_modelled_countries_for('TB')
+    tgf_funding.df = tgf_funding.df[tgf_funding.df.index.isin(list)]
+
+    non_tgf_funding = (
+        NonTgfFunding(
+            path_to_data_folder
+            / "IC8"
+            / "funding"
+            / "2024_11_24"
+            / "tb"
+            / "non_tgf"
+            / "tb_nonfung_base_c.csv"
+        )
+    )
+
+    non_tgf_funding.df = non_tgf_funding.df[non_tgf_funding.df.index.isin(list)]
+
+    return Analysis(
+        database=db,
+        scenario_descriptor='PF',
+        tgf_funding=tgf_funding,
+        non_tgf_funding=non_tgf_funding,
+        parameters=parameters,
+        handle_out_of_bounds_costs=True,
+        innovation_on=False,
+    )
+
+
+if __name__ == "__main__":
+    LOAD_DATA_FROM_RAW_FILES = True
+    DO_CHECKS = False
+
+    # Create the Analysis object
+    analysis = get_tb_analysis(
+        load_data_from_raw_files=LOAD_DATA_FROM_RAW_FILES,
+        do_checks=DO_CHECKS
+    )
+
+    # Get the finalised Set of Portfolio Projections (decided upon IC scenario and Counterfactual):
+    from scripts.ic8.analyses.main_results_for_investment_case import get_set_of_portfolio_projections
+    pps = get_set_of_portfolio_projections(analysis)
+
+    # Get results out from this set for the graph
+    print('hello')
+    filename = 'tb_results_2035.csv'
+    list_of_dfs = list()  # list of mini dataframes for each indicator for each country
+    indicators = ['cases', 'deaths', 'deathshivneg', 'population']
+
+    for country in pps.IC.country_results.keys():
+        y = pps.IC.country_results[country].model_projection
+        years = range(2022, 2036)
+        for indicator in indicators:
+            df = y[indicator][['model_central', 'model_high', 'model_low']].loc[years].reset_index()
+            df['indicator'] = indicator
+            df['country'] = country
+            list_of_dfs.append(df)
+
+        # build whole df for export
+    whole_df = pd.concat(list_of_dfs, axis=0)
+
+    # save to csv
+    whole_df.to_csv(filename, index=False)
+
+
