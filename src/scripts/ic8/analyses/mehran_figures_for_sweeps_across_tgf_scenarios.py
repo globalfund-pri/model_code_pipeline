68b82573a69d8f545603219754b84a9235b1ccd2
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-12-08 18:36:27 +0000

    declare parameters being it's called

--- src/scripts/ic8/analyses/mehran_figures_for_sweeps_across_tgf_scenarios.py
+++ src/scripts/ic8/analyses/mehran_figures_for_sweeps_across_tgf_scenarios.py
@@ -39,8 +39,8 @@ LOAD_DATA = True
 DO_RUN = True
 
 #%% Declare assumptions that are not going to change in the analysis
-SCENARIO_DESCRIPTOR = parameters.get('SCENARIO_DESCRIPTOR_FOR_IC')
 parameters = Parameters(project_root / "src" / "scripts" / "ic8" / "shared" / "parameters.toml")
+SCENARIO_DESCRIPTOR = parameters.get('SCENARIO_DESCRIPTOR_FOR_IC')
 
 
 #%% Load the databases for HIV, Tb and Malaria
